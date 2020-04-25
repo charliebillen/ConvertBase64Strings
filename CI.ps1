6c646a7e91e2c00e697eaeed2e3b7bf75e695a9d
Charlie Billen <charlesbillen@gmail.com>
2020-04-26 00:39:29 +0100

    Forces module installation on CI agent

--- CI.ps1
+++ CI.ps1
@@ -1,4 +1,4 @@
-if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser }
-if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser }
+if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser -Force }
+if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser -Force }
 
 Invoke-psake -buildFile $PSScriptRoot\PsakeTasks.ps1
