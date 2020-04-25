92193232c0aed7d5f3a3515cc3767c5f3b641c32
Charlie Billen <charlesbillen@gmail.com>
2020-04-26 00:46:48 +0100

    Adds PSScriptAnalyzer module to CI and tries to get failing build

--- CI.ps1
+++ CI.ps1
@@ -1,4 +1,7 @@
+if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force }
 if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser -Force }
 if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Scope CurrentUser -Force }
 
 Invoke-psake -buildFile $PSScriptRoot\PsakeTasks.ps1
+
+exit (!$psake.build_success)
